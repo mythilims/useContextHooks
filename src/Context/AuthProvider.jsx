36c0f51e51fddc874aec2070b8c23308f25d116d
mythili <mythilijobofficial@gmail.com>
2025-04-27 19:42:45 +0530

    code changes

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -3,25 +3,27 @@ import { useNavigate } from "react-router-dom";
 import { UserAuthDetails } from "./AuthCon";
 export function AuthProvider ({children}){
     const navigate = useNavigate();
-    const [userName,setUserName]=useState();
-    const [user,setUser]=useState();
+    const [userName,setUserName]=useState('');
+    const [user,setUser]=useState('');
     const [selectedBook, setSelectedBook] = useState(null);
     const [psw,setPsw] =useState('');
     const login =()=>{
         localStorage.setItem('userName' ,userName);
         const user = localStorage.getItem('userName');
         setUser(user);
-        navigate('/das')
+        navigate('/das');
     }
     useEffect(()=>{
       const user = localStorage.getItem('userName');
-      setUser(user)
+      setUser(user);
     },[])
     const logOut = () =>{
         localStorage.clear();
+        setPsw('');
+        setUserName('');
         setUser('');
         setSelectedBook(null);
-        navigate('/')
+        navigate('/');
     }
     
     
